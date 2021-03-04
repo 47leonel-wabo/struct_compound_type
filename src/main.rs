0217243091ff03c53fb50f16a14f1a63c7c9b4c1
leonel47 <waboleonel@gmail.com>
2021-03-03 23:18:05 -0500

    Adding age category method

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ fn main() {
         String::from("Aya Mokoko"),
         22 as i8,
         String::from("Manager"),
-        Address::create(
+        Address::new(
             "25 st-green".to_string(),
             String::from("Dokoti"),
             String::from("Douala"),
         ),
     );
 
-    println!("{:?}", aya)
+    println!("{:#?}", aya);
+    println!("{:?} is adult: {}", aya.name, aya.is_adult());
 }
 
 #[derive(Debug)]
@@ -21,7 +22,7 @@ struct Address {
 }
 
 impl Address {
-    fn create(street: String, town: String, city: String) -> Address {
+    fn new(street: String, town: String, city: String) -> Address {
         Address { street, town, city }
     }
 }
@@ -38,4 +39,5 @@ impl Person {
     fn new(name: String, age: i8, occupation: String, address: Address) -> Person {
         Person { name, age, occupation, address }
     }
+    fn is_adult(&self) -> bool { self.age >= 18 }
 }
